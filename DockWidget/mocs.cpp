ba7c47fa103fbcf89b0096f98ecbe15a2c1120ff
Johannes Sixt <johannes.sixt@telecom.at>
2000-02-20 17:40:29 +0000

    "make dist" must not redistribute moc files.

---
+++ DockWidget/mocs.cpp
@@ -0,0 +1,9 @@
+// $Id$
+
+// This file only includes all moc-generated files.
+
+#include "dockmainwindow.moc"
+#include "dockmanager.moc"
+#include "dockmovemanager.moc"
+#include "docksplitter.moc"
+#include "knewpanner.moc"
---
+++ STabCtl/mocs.cpp
@@ -0,0 +1,6 @@
+// $Id$
+
+// This file only includes all moc-generated files.
+
+#include "stabbar.moc"
+#include "stabctl.moc"
