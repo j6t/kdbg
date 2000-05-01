e48cc2d7e08f120040d18f579321a19de81c081b
Johannes Sixt <johannes.sixt@telecom.at>
2000-05-01 19:46:38 +0000

    Use KCmdLineArgs instead of getopt for KDE2.

--- kdbg/main.cpp
+++ kdbg/main.cpp
@@ -7,26 +7,12 @@
 #include <kapp.h>
 #if QT_VERSION >= 200
 #include <klocale.h>			/* i18n */
-/*
- * Presence or absence of I18N_NOOP distinguishes between KRASH release and
- * "modern" KDE 2.
- */
-#ifdef I18N_NOOP
-# define HAVE_KDE2_POST_KRASH 1
-#else
-# undef HAVE_KDE2_POST_KRASH
-#endif
-
 #include <kmessagebox.h>
 #include <kglobal.h>
 #include <kstddirs.h>
-
-#ifdef HAVE_KDE2_POST_KRASH
-# include <kcmdlineargs.h> 
-# include <kaboutdata.h>
-# include <khelpmenu.h> 
-#endif
-
+#include <kcmdlineargs.h> 
+#include <kaboutdata.h>
+#include <khelpmenu.h> 
 #else // QT_VERSION < 200
 #include <kmsgbox.h>
 #endif
@@ -52,7 +38,7 @@
 
 int main(int argc, char** argv)
 {
-#ifndef HAVE_KDE2_POST_KRASH
+#if QT_VERSION < 200
     KApplication app(argc, argv, "kdbg");
 
     QString about = "KDbg " VERSION " - ";
@@ -76,14 +62,17 @@ int main(int argc, char** argv)
 			i18n("Docking windows"),
 			"novaprint@mtu-net.ru");
     KCmdLineArgs::init( argc, argv, &aboutData );
+
+    static KCmdLineOptions options[] = {
+	{ "t <file>", I18N_NOOP("transcript of conversation with the debugger"), 0 },
+	{ "r <device>", I18N_NOOP("remote debugging via <device>"), 0 },
+	{ "+[program]", I18N_NOOP("path of executable to debug"), 0 },
+	{ "+[core]", I18N_NOOP("a core file to use"), 0}
+    };
+    KCmdLineArgs::addCmdLineOptions(options);
     
     KApplication app;
-#endif
-    extern char *optarg;
-    extern int optind;
-    int ch;
 
-#if QT_VERSION >= 200
     KGlobal::dirs()->addResourceType("types", "share/apps/kdbg/types");
 #endif
 
@@ -93,13 +82,9 @@ int main(int argc, char** argv)
 
     // insert help menu
     QPopupMenu* helpMenu;
-#ifndef HAVE_KDE2_POST_KRASH
-# if QT_VERSION < 200
+#if QT_VERSION < 200
     helpMenu = kapp->getHelpMenu(false, about);
-# else
-    helpMenu = debugger.helpMenu(about, false);
-# endif
-#else // KDE2 post-KRASH
+#else
     KHelpMenu* khm = new KHelpMenu(&debugger, &aboutData, false);
     helpMenu = khm->menu();
 #endif
@@ -122,7 +107,13 @@ int main(int argc, char** argv)
 
     debugger.show();
 
-    // handle optional arguments
+    // handle options
+
+#if QT_VERSION < 200
+    extern char *optarg;
+    extern int optind;
+    int ch;
+
     const char* transcript = 0;
     while ((ch = getopt(argc, argv, "r:t:")) != -1) {
 	switch (ch) {
@@ -138,13 +129,21 @@ int main(int argc, char** argv)
     }
     argc -= optind - 1;
     argv += optind - 1;
-
+#else
+    KCmdLineArgs *args = KCmdLineArgs::parsedArgs();
+    QCString t = args->getOption("t");
+    const char* transcript = t.data();
+    QString remote = args->getOption("r");
+    if (!remote.isEmpty())
+	debugger.setRemoteDevice(remote);
+#endif
     // check environment variable for transcript file name
     if (transcript == 0) {
 	transcript = getenv("KDBG_TRANSCRIPT");
     }
     debugger.setTranscript(transcript);
 
+#if QT_VERSION < 200
     if (!restored && argc > 1) {
 	// check for core file
 	if (argc > 2) {
@@ -153,17 +152,27 @@ int main(int argc, char** argv)
 	if (!debugger.debugProgram(argv[1])) {
 	    // failed
 	    TRACE("cannot start debugger");
-#if QT_VERSION < 200
 	    KMsgBox::message(&debugger, kapp->appName(),
 			     i18n("Cannot start debugger."),
 			     KMsgBox::STOP,
 			     i18n("OK"));
-#else
-	    KMessageBox::error(&debugger, i18n("Cannot start debugger."));
-#endif
 	    debugger.setCoreFile("");
 	}
     }
+#else
+    if (!restored && args->count() > 0) {
+	// check for core file
+	if (args->count() > 1) {
+	    debugger.setCoreFile(args->arg(1));
+	}
+	if (!debugger.debugProgram(args->arg(0))) {
+	    // failed
+	    TRACE("cannot start debugger");
+	    KMessageBox::error(&debugger, i18n("Cannot start debugger."));
+	    debugger.setCoreFile("");
+	}
+    }
+#endif
 
     int rc = app.exec();
     return rc;
