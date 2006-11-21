5652bac3e213e3576262377e2434a1bf893ebcca
Johannes Sixt <johannes.sixt@telecom.at>
2006-11-21 22:16:31 +0100

    Extend the widechar test by a structure with wchar_t* members.

--- kdbg/testprogs/widechar.cpp
+++ kdbg/testprogs/widechar.cpp
@@ -2,6 +2,11 @@
 #include <stdio.h>
 #include <string>
 
+struct WChar {
+	const wchar_t* cwstr;
+	wchar_t* wstr;
+};
+
 int main()
 {
    int j=3;
@@ -10,6 +15,10 @@ int main()
    const wchar_t* str = L"abc";
    wchar_t* str2 = L"def";
    const char* shortStr = "12345";
+
+   WChar s = { 0, L"Some string" };
+   s.cwstr = s.wstr;
+
    wchar_t wstr[64], *wstrPtr = wstr;
 
    wprintf(L"wide string: %S\n", str);
