ae6f2724eb4c57db0b1676ad7f4b19b120f8744f
Johannes Sixt <j6t@kdbg.org>
2008-11-19 22:20:13 +0100

    Add some tests that involve std::vector.
    We are about to add support for templates. std::vector will be our
    guinea pig.

--- kdbg/testprogs/std.cpp
+++ kdbg/testprogs/std.cpp
@@ -1,9 +1,18 @@
 #include <string>
+#include <vector>
 #include <iostream>
+
+void test_vector(std::vector<std::string>& v)
+{
+	v.push_back("xyz");
+}
 
 int main()
 {
 	std::string s = "abc";
+	std::vector<std::string> v(10, s);
+
+	test_vector(v);
 
 	std::cout << s << std::endl;
 }
