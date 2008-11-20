30333d1aa0236df7779da755a51b1506c5a082e4
Johannes Sixt <j6t@kdbg.org>
2008-11-20 22:58:31 +0100

    More tests with std::vector.
    This time with a class derived from std::vector in order to test that
    the derived class is registered as an alias of std::vector.

--- kdbg/testprogs/std.cpp
+++ kdbg/testprogs/std.cpp
@@ -2,6 +2,16 @@
 #include <vector>
 #include <iostream>
 
+template<typename T>
+struct V : std::vector<T>
+{
+	V(const T& v) : std::vector<T>(10, v) {}
+	void anotherone(const T& v)
+	{
+		push_back(v);
+	}
+};
+
 void test_vector(std::vector<std::string>& v)
 {
 	v.push_back("xyz");
@@ -10,9 +20,10 @@ void test_vector(std::vector<std::string>& v)
 int main()
 {
 	std::string s = "abc";
-	std::vector<std::string> v(10, s);
+	V<std::string> v(s);
 
 	test_vector(v);
+	v.anotherone("ABC");
 
 	std::cout << s << std::endl;
 }
