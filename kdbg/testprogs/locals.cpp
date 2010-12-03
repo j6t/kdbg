b8d743387ef78ee1655b6c201b429a51371324a0
Johannes Sixt <j6t@kdbg.org>
2010-12-03 22:27:48 +0100

    Build test programs.
    We do not use a build system for this task because portability is not a
    concern.
    
    Some compiler warnings about const-ness of string constants are fixed.

--- kdbg/testprogs/locals.cpp
+++ kdbg/testprogs/locals.cpp
@@ -3,7 +3,7 @@
 
 // a function that has args but no locals
 
-static int nolocals(int argc, char** argv)
+static int nolocals(int argc, const char** argv)
 {
 	printf("argc=%d, argv[0]=%s\n", argc, argv[0]);
 }
@@ -14,12 +14,12 @@ static int nolocals(int argc, char** argv)
 static int noargs()
 {
 	int c = 1;
-	char* pgm[] = { "foo", 0 };
+	const char* pgm[] = { "foo", 0 };
 	nolocals(c, pgm);
 }
 
 
-int main(int argc, char** argv)
+int main(int argc, const char** argv)
 {
 	noargs();
 	nolocals(argc, argv);
--- kdbg/testprogs/widechar.cpp
+++ kdbg/testprogs/widechar.cpp
@@ -13,13 +13,13 @@ int main()
    const wchar_t* nullPtr = 0;
    const wchar_t* uninitializedPtr = (const wchar_t*)0xdeadbeef;
    const wchar_t* str = L"abc";
-   wchar_t* str2 = L"def";
+   const wchar_t* str2 = L"def";
    const char* shortStr = "12345";
 
-   WChar s = { 0, L"Some string" };
-   s.cwstr = s.wstr;
+   wchar_t wstr[64] = L"Some string", *wstrPtr = wstr;
 
-   wchar_t wstr[64], *wstrPtr = wstr;
+   WChar s = { 0, wstr };
+   s.cwstr = s.wstr;
 
    wprintf(L"wide string: %S\n", str);
 
