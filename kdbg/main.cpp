9146be31d80c944a0fa770b978faf25ab7c15407
Johannes Sixt <j6t@kdbg.org>
2014-02-22 20:56:44 +0100

    Extend copyright to 2014.

--- kdbg/main.cpp
+++ kdbg/main.cpp
@@ -24,7 +24,7 @@ int main(int argc, char** argv)
 			 KDBG_VERSION,
 			 ki18n("A Debugger"),
 			 KAboutData::License_GPL, 
-			 ki18n("(c) 1998-2013 Johannes Sixt"),
+			 ki18n("(c) 1998-2014 Johannes Sixt"),
 			 KLocalizedString(),	/* any text */
 			 "http://www.kdbg.org/",
 			 "j6t@kdbg.org");
