7a9578bac79245723a68178deaa41166dfd73604
Johannes Sixt <j6t@kdbg.org>
2023-11-01 13:40:38 +0100

    Do not use deprecated QSet<>::toList().

--- kdbg/debugger.cpp
+++ kdbg/debugger.cpp
@@ -775,7 +775,7 @@ void KDebugger::saveProgramSettings()
     gg.writeEntry(FileVersion, 1);
     gg.writeEntry(ProgramArgs, m_programArgs);
     gg.writeEntry(WorkingDirectory, m_programWD);
-    gg.writeEntry(OptionsSelected, m_boolOptions.toList());
+    gg.writeEntry(OptionsSelected, m_boolOptions.values());
     gg.writeEntry(DebuggerCmdStr, m_debuggerCmd);
     gg.writeEntry(TTYLevelEntry, int(m_ttyLevel));
     gg.writeEntry(DisassemblyFlavor, m_flavor);
