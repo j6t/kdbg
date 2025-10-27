23c519e48972239102949735a57f826076b327c0
Sebastian Pipping <sebastian@pipping.org>
2025-10-27 21:43:06 +0100

    kdbg/dbgmainwnd.cpp: Adress warning about QKeyCombination::operator int() const
    The symptom was:
    > [..]/kdbg/dbgmainwnd.cpp:318:37: warning: ‘constexpr QKeyCombination::operator int() const’ is deprecated: Use QKeyCombination instead of int [-Wdeprecated-declarations]
    >   318 |     m_stepIntoIAction = createAction(i18n("Step i&nto by instruction"),
    >       |                         ~~~~~~~~~~~~^~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
    >   319 |                         "debug-step-into-instruction", Qt::SHIFT | Qt::Key_F8,
    >       |                         ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
    >   320 |                         m_debugger, SLOT(programStepi()), "exec_step_into_by_insn");
    >       |                         ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
    > /usr/include/qt6/QtCore/qnamespace.h:1920:26: note: declared here
    >  1920 |     constexpr Q_IMPLICIT operator int() const noexcept
    >       |                          ^~~~~~~~
    > [..]/kdbg/dbgmainwnd.cpp:322:37: warning: ‘constexpr QKeyCombination::operator int() const’ is deprecated: Use QKeyCombination instead of int [-Wdeprecated-declarations]
    >   322 |     m_stepOverIAction = createAction(i18n("Step o&ver by instruction"),
    >       |                         ~~~~~~~~~~~~^~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
    >   323 |                         "debug-step-instruction", Qt::SHIFT | Qt::Key_F10,
    >       |                         ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
    >   324 |                         m_debugger, SLOT(programNexti()), "exec_step_over_by_insn");
    >       |                         ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
    > /usr/include/qt6/QtCore/qnamespace.h:1920:26: note: declared here
    >  1920 |     constexpr Q_IMPLICIT operator int() const noexcept
    >       |                          ^~~~~~~~
    > [..]/kdbg/dbgmainwnd.cpp:345:37: warning: ‘constexpr QKeyCombination::operator int() const’ is deprecated: Use QKeyCombination instead of int [-Wdeprecated-declarations]
    >   345 |     m_bpSetTempAction = createAction(i18n("Set &temporary breakpoint"), Qt::SHIFT | Qt::Key_F9,
    >       |                         ~~~~~~~~~~~~^~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
    >   346 |                         m_filesWindow, SLOT(slotBrkptSetTemp()), "breakpoint_set_temporary");
    >       |                         ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
    > /usr/include/qt6/QtCore/qnamespace.h:1920:26: note: declared here
    >  1920 |     constexpr Q_IMPLICIT operator int() const noexcept
    >       |                          ^~~~~~~~
    > [..]/kdbg/dbgmainwnd.cpp:347:36: warning: ‘constexpr QKeyCombination::operator int() const’ is deprecated: Use QKeyCombination instead of int [-Wdeprecated-declarations]
    >   347 |     m_bpEnableAction = createAction(i18n("&Enable/Disable breakpoint"), Qt::CTRL | Qt::Key_F9,
    >       |                        ~~~~~~~~~~~~^~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
    >   348 |                         m_filesWindow, SLOT(slotBrkptEnable()), "breakpoint_enable");
    >       |                         ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
    > /usr/include/qt6/QtCore/qnamespace.h:1920:26: note: declared here
    >  1920 |     constexpr Q_IMPLICIT operator int() const noexcept
    >       |                          ^~~~~~~~

--- kdbg/dbgmainwnd.cpp
+++ kdbg/dbgmainwnd.cpp
@@ -28,6 +28,9 @@
 #include <QFileInfo>
 #include <QGuiApplication>
 #include <QIcon>
+#if QT_VERSION >= QT_VERSION_CHECK(6, 0, 0)
+# include <QKeyCombination>
+#endif
 #include <QList>
 #include <QDockWidget>
 #include <QProcess>
@@ -315,13 +318,25 @@ void DebuggerMainWnd::initKAction()
 			"debug-execute-to-cursor", Qt::Key_F7,
 			this, SLOT(slotExecUntil()), "exec_run_to_cursor");
     connect(m_toCursorAction, SIGNAL(triggered()), this, SLOT(intoBackground()));
+#if QT_VERSION >= QT_VERSION_CHECK(6, 0, 0)
+    m_stepIntoIAction = createAction(i18n("Step i&nto by instruction"),
+			"debug-step-into-instruction", QKeyCombination(Qt::SHIFT, Qt::Key_F8).toCombined(),
+			m_debugger, SLOT(programStepi()), "exec_step_into_by_insn");
+#else
     m_stepIntoIAction = createAction(i18n("Step i&nto by instruction"),
 			"debug-step-into-instruction", Qt::SHIFT | Qt::Key_F8,
 			m_debugger, SLOT(programStepi()), "exec_step_into_by_insn");
+#endif
     connect(m_stepIntoIAction, SIGNAL(triggered()), this, SLOT(intoBackground()));
+#if QT_VERSION >= QT_VERSION_CHECK(6, 0, 0)
+    m_stepOverIAction = createAction(i18n("Step o&ver by instruction"),
+			"debug-step-instruction", QKeyCombination(Qt::SHIFT, Qt::Key_F10).toCombined(),
+			m_debugger, SLOT(programNexti()), "exec_step_over_by_insn");
+#else
     m_stepOverIAction = createAction(i18n("Step o&ver by instruction"),
 			"debug-step-instruction", Qt::SHIFT | Qt::Key_F10,
 			m_debugger, SLOT(programNexti()), "exec_step_over_by_insn");
+#endif
     connect(m_stepOverIAction, SIGNAL(triggered()), this, SLOT(intoBackground()));
     m_execMovePCAction = createAction(i18n("&Program counter to current line"),
 			"debug-run-cursor", 0,
@@ -342,10 +357,17 @@ void DebuggerMainWnd::initKAction()
     // breakpoint menu
     m_bpSetAction = createAction(i18n("Set/Clear &breakpoint"), "brkpt", Qt::Key_F9,
 			m_filesWindow, SLOT(slotBrkptSet()), "breakpoint_set");
+#if QT_VERSION >= QT_VERSION_CHECK(6, 0, 0)
+    m_bpSetTempAction = createAction(i18n("Set &temporary breakpoint"), QKeyCombination(Qt::SHIFT, Qt::Key_F9).toCombined(),
+			m_filesWindow, SLOT(slotBrkptSetTemp()), "breakpoint_set_temporary");
+    m_bpEnableAction = createAction(i18n("&Enable/Disable breakpoint"), QKeyCombination(Qt::CTRL, Qt::Key_F9).toCombined(),
+			m_filesWindow, SLOT(slotBrkptEnable()), "breakpoint_enable");
+#else
     m_bpSetTempAction = createAction(i18n("Set &temporary breakpoint"), Qt::SHIFT | Qt::Key_F9,
 			m_filesWindow, SLOT(slotBrkptSetTemp()), "breakpoint_set_temporary");
     m_bpEnableAction = createAction(i18n("&Enable/Disable breakpoint"), Qt::CTRL | Qt::Key_F9,
 			m_filesWindow, SLOT(slotBrkptEnable()), "breakpoint_enable");
+#endif
 
     // only in popup menus
     createAction(i18n("Watch Expression"), 0,
