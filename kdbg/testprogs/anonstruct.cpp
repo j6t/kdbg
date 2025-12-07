ab3e10ad4761747688832c87320b3e61b2b0c384
Johannes Sixt <j6t@kdbg.org>
2025-12-07 12:54:56 +0100

    A test case where the anonymous union is the first member.
    Our parser can recognize anonymous unions only when they are not the first
    member of the enclosing structure. Provide a test case that does have an
    anonymous union as the first member, so that we can test the parser as we
    are going to improve it.

--- kdbg/testprogs/anonstruct.cpp
+++ kdbg/testprogs/anonstruct.cpp
@@ -1,6 +1,7 @@
 // test anonymous structs and unions
 
 #include <cstdio>
+#include <cstring>
 #include <pthread.h>
 
 struct T {
@@ -18,6 +19,18 @@ struct T {
 	}
 };
 
+struct UnionFirst {
+	union {
+		char shortname[32];
+		const char* longname;
+	};
+	struct {
+		int second;
+		double third;
+	};
+	int regular;
+};
+
 int main()
 {
 	pthread_mutex_t mutex = PTHREAD_MUTEX_INITIALIZER;
@@ -26,7 +39,13 @@ int main()
 		char a;
 		int b;
 	};
+	UnionFirst u = {};
+
 	a = 'X';
 	b = t.TestPopup();
 	std::fprintf(stderr, "%zu, %zu, b=%d\n", sizeof(mutex), sizeof(t), b);
+
+	std::strcpy(u.shortname, "short!");
+	u.third = 3.14;
+	std::fprintf(stderr, "u.shortname=%s\n", u.shortname);
 }
